a8fd48b089ee722219cb1e93b361b88263e60c24
David Dada <daveproxy80@gmail.com>
2025-08-10 23:03:25 +0100

    chore: remove unused imports

--- test/DiamondTester.t.sol
+++ test/DiamondTester.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import {Test} from "forge-std/Test.sol";
 import {Facet} from "@diamond-storage/DiamondStorage.sol";
 import {DeployedDiamondState} from "@diamond-test/helpers/TestStates.sol";
 
--- test/helpers/TestStates.sol
+++ test/helpers/TestStates.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import {Test} from "forge-std/Test.sol";
 import {DeployDiamondHelper} from "@diamond-test/helpers/DeployDiamondHelper.sol";
-import {Diamond} from "@diamond/Diamond.sol";
 import {DiamondCutFacet} from "@diamond/facets/DiamondCutFacet.sol";
 import {DiamondLoupeFacet} from "@diamond/facets/DiamondLoupeFacet.sol";
 import {OwnableRolesFacet} from "@diamond/facets/OwnableRolesFacet.sol";
