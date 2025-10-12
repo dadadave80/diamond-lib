02085b258d2b537c1933a36a73058640fb1edcfe
David Dada <daveproxy80@gmail.com>
2025-10-12 13:23:38 +0100

    Update deployed diamond state

--- test/states/DeployedDiamondState.sol
+++ test/states/DeployedDiamondState.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.20;
 
 import {DeployDiamond} from "@diamond-script/DeployDiamond.s.sol";
-import {DiamondCutFacet} from "@diamond/facets/DiamondCutFacet.sol";
-import {DiamondLoupeFacet} from "@diamond/facets/DiamondLoupeFacet.sol";
 import {GetSelectors} from "@diamond-test/helpers/GetSelectors.sol";
+import {DiamondLoupeFacet} from "@diamond/facets/DiamondLoupeFacet.sol";
 import {OwnableRolesFacet} from "@diamond/facets/OwnableRolesFacet.sol";
+import {IDiamondCut} from "@diamond/interfaces/IDiamondCut.sol";
 
 /// @notice Provides shared state for tests involving a freshly deployed Diamond contract.
 /// @dev Sets up references to deployed facets, interfaces, and the diamond itself for testing.
@@ -15,7 +15,7 @@ abstract contract DeployedDiamondState is GetSelectors {
     address public diamond;
 
     /// @notice Interface for the DiamondCut functionality of the deployed diamond.
-    DiamondCutFacet public diamondCut;
+    IDiamondCut public diamondCut;
 
     /// @notice Interface for the DiamondLoupe functionality of the deployed diamond.
     DiamondLoupeFacet public diamondLoupe;
@@ -29,7 +29,7 @@ abstract contract DeployedDiamondState is GetSelectors {
     /// @notice List of facet contract names used in deployment.
     string[3] public facetNames = ["DiamondCutFacet", "DiamondLoupeFacet", "OwnableRolesFacet"];
 
-    // address public diamondOwner = makeAddr("Owner");
+    address public diamondOwner = address(this);
 
     /// @notice Deploys the Diamond contract and initializes interface references and facet addresses.
     /// @dev This function is intended to be called in a test setup phase (e.g., `setUp()` in Foundry).
@@ -37,7 +37,7 @@ abstract contract DeployedDiamondState is GetSelectors {
         deployDiamond = new DeployDiamond();
         diamond = deployDiamond.run();
 
-        diamondCut = DiamondCutFacet(diamond);
+        diamondCut = IDiamondCut(diamond);
         diamondLoupe = DiamondLoupeFacet(diamond);
         ownableRoles = OwnableRolesFacet(diamond);
 
